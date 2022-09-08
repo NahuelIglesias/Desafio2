576ebd0d0e4a6239644932dcdb223ff599203711
NahuelIglesias <nahueliglesias1999@gmail.com>
2022-09-08 01:58:42 -0300

    Added turrets

--- Desafio2/scenes/first_world.gd
+++ Desafio2/scenes/first_world.gd
@@ -2,3 +2,4 @@ extends Node2D
 
 func _ready():
 	$tank_player.set_bullet_container(self)
+	$turret.set_tank_player()
---
+++ Desafio2/scripts/turret.gd
@@ -0,0 +1,25 @@
+extends Sprite
+
+export (PackedScene) var bullet_scene:PackedScene
+onready var shoot_position:Position2D = $ShootPosition
+var bullet_container:Node
+
+var tank_player
+
+func set_values(tank_player, bullet_container):
+	self.tank_player = tank_player
+	self.bullet_container = bullet_container
+	$Timer.start()
+
+func _on_Timer_timeout():
+	shoot()
+
+func shoot():
+	var bullet:bullet = bullet_scene.instance()
+	bullet_container = add_child(bullet)
+	bullet.set_starting_values(shoot_position.global_position, (tank_player.global_position - shoot_position.global_position).normalized())
+	bullet.connect("delete_requested", self, "on_bullet_delete_requested")
+
+func on_bullet_delete_requested(bullet):
+	bullet_container.remove_child(bullet)
+	bullet.queue_free()
